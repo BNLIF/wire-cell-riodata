dfc9a93bbaf9824b004cc55fc6327d5400db0864
Brett Viren <bv@bnl.gov>
2015-07-06 17:54:43 -0400

    First commit

---
+++ tests/test_rootdataio.cxx
@@ -0,0 +1,17 @@
+#include "WireCellRioData/BlobCollection.h"
+#include "WireCellRioData/Blob.h"
+#include "WireCellRioData/CellCharge.h"
+#include "WireCellRioData/Cell.h"
+#include "WireCellRioData/ChannelCharge.h"
+#include "WireCellRioData/ChannelSlice.h"
+#include "WireCellRioData/FrameStore.h"
+#include "WireCellRioData/GeometryStore.h"
+#include "WireCellRioData/Point.h"
+#include "WireCellRioData/Wire.h"
+
+int main()
+{
+    
+
+    return 0;
+}
